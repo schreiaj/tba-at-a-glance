695a34768720672ff1b8906d405ff23a957bab28
Andrew Schreiber <schreiaj@gmail.com>
2017-09-20 13:05:41 -0400

    make it use https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	TBA_KEY : process.env.REACT_APP_TBA_KEY,
-	BASE_URL :'http://www.thebluealliance.com/api/v3',
+	BASE_URL :'https://www.thebluealliance.com/api/v3',
 	APP_NAME: 'tba-at-a-glance',
 	EVENT_TYPES: [0,1]
 }
