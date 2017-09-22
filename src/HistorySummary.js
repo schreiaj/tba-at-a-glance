ce32766b8983771d414c34927c4da4f47b4a4bdf
Andrew Schreiber <schreiaj@gmail.com>
2017-09-21 23:21:47 -0400

    fixed grid size, added labels

--- src/HistorySummary.js
+++ src/HistorySummary.js
@@ -9,15 +9,36 @@ let HistorySummary = ({team, events, eventResults}) => {
 	let years = Array.from(new Set(events.map((e) => new Date(e.start_date).getFullYear())))
 
 	let eventsByYear = nest().key((e) => new Date(e.start_date).getFullYear())
-		// .sortKeys((a,b) => new Date(b.start_date) - new Date(a.start_date))
+		.key((e) => {
+			switch (e.event_type) {
+			case 0:
+				return 1
+				break;
+			case 2:
+				return 2
+				break;
+			default:
+				return e.event_type
+			}
+		})
+		.sortValues((a,b) => new Date(a.start_date) - new Date(b.start_date))
 		.object(events)
 	window.eventsByYear = eventsByYear;
 
-	let style = {display: 'grid', gridTemplateColumns: `${'1fr '.repeat(years.length)}`, gridColumnGap: '5px'};
+	let style = {display: 'grid', gridTemplateColumns: `2fr ${'1fr '.repeat(years.length)}`, gridColumnGap: '5px'};
 
 	return(
 		<div className='history-summary'>
 			<div style={style}>
+				<div>
+					<div>.</div>
+					<div className='label event'>Event 1</div>
+					<div className='label event'>Event 2</div>
+					<div className='label event'>Event 3 or DCMP</div>
+					<div className='label event'>CMP Division</div>
+					<div className='label event'>Einstein</div>
+					<div className='label event'>FoC</div>
+				</div>
 				{years.map((y) => {
 					return (<div className='year'key={y}>{y}
 						<YearSummary year={y} eventResults={eventResults} events={eventsByYear} />
@@ -41,10 +62,27 @@ let HistorySummary = ({team, events, eventResults}) => {
 
 const YearSummary = ({events, year, eventResults}) => {
 
-	let yearsEvents = events[year].sort((a,b) => new Date(a.start_date) - new Date(b.start_date));
+	let eventYear = events[year]||[];
+	let yearsEvents = []
+	yearsEvents.push((eventYear[1]||[]).pop());
+	yearsEvents.push((eventYear[1]||[]).pop());
+	if(eventYear[2]) {
+		yearsEvents.push((eventYear[2]||[]).pop());
+	} else {
+		yearsEvents.push((eventYear[1]||[]).pop());
+	}
+	yearsEvents.push((eventYear[3]||[]).pop());
+	yearsEvents.push((eventYear[4]||[]).pop());
+	yearsEvents.push((eventYear[6]||[]).pop());
+
+
+
 	return (
 		<div key={year}>
-		{yearsEvents.map((e) => {
+		{yearsEvents.map((e,i) => {
+					if(!e) {
+						return <div className='event' key={i}></div>;
+					}
 					return (
 						<EventSummary key={e.key} eventKey={e.key} eventResults={eventResults} />
 					);
@@ -56,7 +94,7 @@ const YearSummary = ({events, year, eventResults}) => {
 const EventSummary = ({eventKey, eventResults}) => {
 	let results = eventResults[eventKey];
 		if(results) {
-			let {playoff, alliance} = results;
+			let {playoff, alliance, qual} = results;
 			const pickPos = {0: 'C', 1: '1', 2: '2', 3: 'B'};
 			// const elimFinish = {'ef': 'Eightfinalist', 'qf': 'Quarterfinalist', 'sf': 'Semifinalist', 'f': 'Finalist'}
 			let playoffResult = null;
@@ -67,11 +105,13 @@ const EventSummary = ({eventKey, eventResults}) => {
 				playoffResult = (playoff||{}).level
 			}
 			if(playoff && alliance) {
-				return <div className={`event ${playoffResult}`} key={eventKey}><span className='content'>A{alliance.number}P{pickPos[alliance.pick]}</span></div>
+				return <div onClick={() => console.log(results)} className={`event ${playoffResult}`} key={eventKey}><span className='content'>A{alliance.number}P{pickPos[alliance.pick]}</span></div>
 			} else if(playoff && !alliance) {
-				return <div className={`event ${playoffResult}`} key={eventKey}><span className='content'>Played*</span></div>
+				return <div onClick={() => console.log(results)} className={`event ${playoffResult}`} key={eventKey}><span className='content'>Played*</span></div>
+			} else if(qual) {
+				return <div onClick={() => console.log(results)} className='event np' key={eventKey}>NP</div>
 			} else {
-				return <div className='event np' key={eventKey}>NP</div>
+				return <div onClick={() => console.log(results)} className='event' key={eventKey}></div>
 			}
 		}
 	return <div className='event placeholder' key={eventKey}>loading...</div>;
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
 	TBA_KEY : process.env.REACT_APP_TBA_KEY,
 	BASE_URL :'https://www.thebluealliance.com/api/v3',
 	APP_NAME: 'tba-at-a-glance',
-	EVENT_TYPES: [0,1],
+	EVENT_TYPES: [0,1,2,3,4,6],
 	MIN_YEAR: 2007
 }
